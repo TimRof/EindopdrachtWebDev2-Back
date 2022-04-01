d5ed71bff5a611b99d437ec005637b0839324eb3
TimRof <70700788+TimRof@users.noreply.github.com>
2022-04-01 22:24:17 +0200

    added exp to jwt

--- app/controllers/logincontroller.php
+++ app/controllers/logincontroller.php
@@ -30,11 +30,13 @@ class LoginController extends Controller
                 }
 
                 $key = "thisismysecretthingy";
+                $tokenExpiration = time() + 5000;
                 $payload = array(
                     "iss" => "http://localhost", // issuer = domein naam van website
                     "aud" => "http://localhost", // checker van token
                     "iat" => time(), // issued at
                     "nbf" => time(), // not before
+                    "exp" => $tokenExpiration, // not before
                     "data" => array(
                         "id" => $user->id,
                         "email" => $user->name,
@@ -43,7 +45,7 @@ class LoginController extends Controller
                 );
                 $jwt = JWT::encode($payload, $key, 'HS256');
                 $admin = $user->admin ? true : false;
-                $this->respond(["token" => $jwt, "id" => $user->id, "email" => $user->email, "name" => $user->name, "admin" => $admin]);
+                $this->respond(["token" => $jwt, "id" => $user->id, "email" => $user->email, "name" => $user->name, "admin" => $admin, "tokenExpiration" => $tokenExpiration]);
             } catch (Exception $e) {
                 $this->respondWithError(500, $e->getMessage());
             }
